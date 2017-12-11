443180d73a5b0307d95df171bff51c65ee81f31c
Leigh McCulloch <leigh@mcchouse.com>
2017-12-11 05:42:22 +0000

    2017-day11-satisfies-all-cases

--- 2017/11--hex-ed/solution.go
+++ 2017/11--hex-ed/solution.go
@@ -1,33 +1,36 @@
 // Package solution contains solutions to the problems described at http://adventofcode.com/2017/day/11.
 package solution
 
-import "strings"
+import (
+	"math"
+	"strings"
+)
 
-var directions = map[string]func(x, y int) (int, int){
-	"n": func(x, y int) (int, int) {
+var directions = map[string]func(x, y float64) (float64, float64){
+	"n": func(x, y float64) (float64, float64) {
 		return x, y + 1
 	},
-	"ne": func(x, y int) (int, int) {
-		return x + 1, y + 1
+	"ne": func(x, y float64) (float64, float64) {
+		return x + 1, y + 0.5
 	},
-	"se": func(x, y int) (int, int) {
-		return x + 1, y - 1
+	"se": func(x, y float64) (float64, float64) {
+		return x + 1, y - 0.5
 	},
-	"s": func(x, y int) (int, int) {
+	"s": func(x, y float64) (float64, float64) {
 		return x, y - 1
 	},
-	"sw": func(x, y int) (int, int) {
-		return x - 1, y - 1
+	"sw": func(x, y float64) (float64, float64) {
+		return x - 1, y - 0.5
 	},
-	"nw": func(x, y int) (int, int) {
-		return x - 1, y + 1
+	"nw": func(x, y float64) (float64, float64) {
+		return x - 1, y + 0.5
 	},
 }
 
 func Part1(input string) int {
 	list := strings.Split(input, ",")
-	x := 0
-	y := 0
+	x := 0.0
+	y := 0.0
 	for _, direction := range list {
 		direction = strings.TrimSpace(direction)
 		if direction == "" {
@@ -36,19 +39,26 @@ func Part1(input string) int {
 		directionFunction := directions[direction]
 		x, y = directionFunction(x, y)
 	}
+	if x < 0.0 {
+		x = -x
+	}
+	if y < 0.0 {
+		y = -y
+	}
 	max := x
 	if y > max {
 		max = y
 	}
-	return max
+	max = math.Ceil(max)
+	return int(max)
 }
 
 func Part2(input string) int {
 	list := strings.Split(input, ",")
-	x := 0
-	y := 0
-	maxX := 0
-	maxY := 0
+	x := 0.0
+	y := 0.0
+	maxX := 0.0
+	maxY := 0.0
 	for _, direction := range list {
 		direction = strings.TrimSpace(direction)
 		if direction == "" {
@@ -63,9 +73,15 @@ func Part2(input string) int {
 			maxY = y
 		}
 	}
+	if maxX < 0 {
+		maxX = -maxX
+	}
+	if maxY < 0 {
+		maxY = -maxY
+	}
 	max := maxX
 	if maxY > max {
 		max = maxY
 	}
-	return max
+	return int(max)
 }
