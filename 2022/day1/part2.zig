65b1ce5835d956bcbd8717dcd55249c72f20c747
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-12-04 21:04:36 -0800

    Remove commented code

--- 2022/day1/part2.zig
+++ 2022/day1/part2.zig
@@ -1,23 +1,6 @@
 const std = @import("std");
 const parseInt = std.fmt.parseInt;
 
-// const input =
-//     \\1000
-//     \\2000
-//     \\3000
-//     \\
-//     \\4000
-//     \\
-//     \\5000
-//     \\6000
-//     \\
-//     \\7000
-//     \\8000
-//     \\9000
-//     \\
-//     \\10000
-// ;
-
 const input = @embedFile("input.txt");
 
 pub fn main() void {
