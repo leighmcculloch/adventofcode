77ecbed7b2b196127831caaed205519fb0d9c45e
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-12-18 22:45:26 -0800

    2022-day6

---
+++ 2022/day6/part2.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+const input = std.mem.trimRight(u8, @embedFile("input.txt"), "\n");
+
+pub fn main() void {
+    const size = 14;
+    var map = [_]usize{0} ** 26;
+    var i: usize = 0;
+    while (i < input.len) : (i += 1) {
+        const c = input[i];
+        map[c - 'a'] += 1;
+        if (i + 1 >= size) {
+            for (map) |cc| {
+                if (cc > 1) {
+                    break;
+                }
+            } else {
+                break;
+            }
+            const sizeAgo = input[i-(size-1)];
+            map[sizeAgo - 'a'] -= 1;
+        }
+    }
+    std.debug.print("count = {d}\n", .{i + 1});
+}
